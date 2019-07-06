98094320553c1327cb348f3f11820389f96ea9c0
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-07-06 09:30:31 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,12 +8,14 @@ class PostsController < ApplicationController
   end
 
   def update
-    
+
     if @post.update(post_params)
+      @post.update(post_params)
       redirect_to post_path(@post)
     else
       render :new
     end
+
   end
 
   private
