d5270653fec174626083442cf82ec16881f26606
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-07-06 09:07:32 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,11 +7,12 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
+    if 
+      @author = Author.create!(author_params)
 
-    redirect_to author_path(@author)
+      redirect_to author_path(@author)
 
-    render :
+      render :
   end
 
   private
